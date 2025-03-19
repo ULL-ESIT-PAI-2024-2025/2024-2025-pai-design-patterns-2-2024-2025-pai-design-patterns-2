174810f442543a556e84a5aa740c1d0df569b67a
AbianSL <abisanle@gmail.com>
2025-03-19 12:18:46 +0000

    add commentaries

--- src/template/classes/template.ts
+++ src/template/classes/template.ts
@@ -11,15 +11,27 @@
 
 abstract class Block {
   public constructor(private name: string, private texture: string, private hardness: number) {}
-
+  
+  /**
+    * Get the name of the block
+    * @returns string - The name of the block
+    */
   public getName(): string {
     return this.name;
   }
 
+  /**
+    * Get the texture of the block
+    * @returns string - The texture of the
+    */
   public getTexture(): string {
     return this.texture;
   }
 
+  /**
+    * Get the hardness of the block
+    * @returns number - The hardness of the block
+    */
   public getHardness(): number {
     return this.hardness;
   }
@@ -34,18 +46,34 @@ abstract class Block {
     this.break();
     this.specialEffect();
   }
-
+  
+  /**
+    * Place the block
+    * @returns void
+    */
   protected place(): void {
     console.log(`Placing ${this.name}`);
   }
-
+  
+  /**
+    * Break the block
+    * @returns void
+    */
   protected break(): void {
     console.log(`Breaking ${this.name}`);
   }
 
+  /**
+    * Special effect from the block
+    * @returns void
+    */
   protected specialEffect(): void {
     console.log(`${this.name} has not special effect`);
   }
-  
+ 
+  /**
+    * get the block when it drops
+    * @returns Block[] - Array of blocks that drops 
+    */
   public abstract getDrops(): Block[];
 }
